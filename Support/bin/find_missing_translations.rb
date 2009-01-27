d0f03ef81c9beacf70963d0b81b0eaa9ddab6d26
Dirk Breuer <dirk.breuer@gmail.com>
2009-01-27 15:21:44 +0100

    CHANGED - finds now even translations with a missing scope

--- Support/bin/find_missing_translations.rb
+++ Support/bin/find_missing_translations.rb
@@ -6,8 +6,6 @@ require "#{ENV["TM_SUPPORT_PATH"]}/lib/web_preview"
 
 require File.join(ENV["TM_PROJECT_DIRECTORY"], "config/environment")
 
-counter = 0
-
 puts html_head(:window_title => "Find Missing Translations", :page_title => 'Find Missing Translations', :sub_title => 'RakeMate')
 
 puts <<-HTML
@@ -18,35 +16,61 @@ puts <<-HTML
 <div style="white-space: normal; -khtml-nbsp-mode: space; -khtml-line-break: after-white-space;"> <!-- Script output -->
 HTML
 
-Dir.glob(File.join(ENV["TM_PROJECT_DIRECTORY"], "app/**/*.{rb,erb}")) do |file|
-  line_counter = 0
+counter = 0
+
+puts "<h1>Scope Missing</h1>"
+
+Dir.glob(File.join(ENV["TM_PROJECT_DIRECTORY"], "app/**/*.{rb,erb}")) do |file_name|
   contains = false
-  File.open(file).each do |line|
-    line_counter += 1
+  file = File.open(file_name)
+  file.each do |line|
+    if line.index(' t(:message')
+      counter += 1
+      contains = true
+      puts "<a href='txmt://open?url=file://#{file_name}&line=#{file.lineno}'>Missing Scope in File: #{File.basename(file_name)}</a> Zeile #{file.lineno}<br />"
+    end
+  end
+  puts "<br />" if contains
+end
+puts "<div>#{counter} Zeilen gefunden</div>"
+
+puts "<br />"
+puts "<hr />"
+puts "<br />"
+
+counter = 0
+
+puts "<h1>Translation Missing</h1>"
+
+Dir.glob(File.join(ENV["TM_PROJECT_DIRECTORY"], "app/**/*.{rb,erb}")) do |file_name|
+  contains = false
+  file = File.open(file_name)
+  file.each do |line|
     if found_translate_call = line.match(/(?:(?:\st|\.t)|translate)\((.*?)\)/i)
       begin
         translation_missing_error = Object.class_eval("I18n.translate(#{found_translate_call[1]})")
         if translation_missing_error.is_a?(Hash)
           counter += 1
           contains = true
-          puts "<a href='txmt://open?url=file://#{file}&line=#{line_counter}'>Missing pluralization parameter in File: #{File.basename(file)}</a> Zeile #{line_counter}<br />"
+          puts "<a href='txmt://open?url=file://#{file_name}&line=#{file.lineno}'>Missing pluralization parameter in File: #{File.basename(file_name)}</a> Zeile #{file.lineno}<br />"
         elsif translation_missing_error.match(/translation missing/)
           counter += 1
           contains = true
-          puts "<a href='txmt://open?url=file://#{file}&line=#{line_counter}'>#{translation_missing_error} in File: #{File.basename(file)}</a> Zeile #{line_counter}<br />"
+          puts "<a href='txmt://open?url=file://#{file_name}&line=#{file.lineno}'>#{translation_missing_error} in File: #{File.basename(file_name)}</a> Zeile #{file.lineno}<br />"
         end
       rescue SyntaxError => se
-        puts "<a style=\"color: red\" href='txmt://open?url=file://#{file}&line=#{line_counter}'>Complex Translation found in File: #{File.basename(file)}.</a> Zeile #{line_counter}<br />"
+        puts "<a style=\"color: red\" href='txmt://open?url=file://#{file_name}&line=#{file.lineno}'>Complex Translation found in File: #{File.basename(file_name)}.</a> Zeile #{file.lineno}<br />"
       rescue NameError => ne
-        puts "<a style=\"color: red\" href='txmt://open?url=file://#{file}&line=#{line_counter}'>Translation which uses variables has been found in File: #{File.basename(file)}.</a> Zeile #{line_counter}<br />"
+        puts "<a style=\"color: red\" href='txmt://open?url=file://#{file_name}&line=#{file.lineno}'>Translation which uses variables has been found in File: #{File.basename(file_name)}.</a> Zeile #{file.lineno}<br />"
       end
     end
   end
   puts "<br />" if contains
 end
 
-
 puts "<div><strong>#{counter}</strong> Zeilen gefunden</div>"
+
+puts "<br />"
 puts "<div class='done'>Done</div>"
 
 puts <<-HTML
